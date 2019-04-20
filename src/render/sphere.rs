91daf78b0c3918b7eb32611056cc4bbf6c45ca68
blargg <tomjankauski@gmail.com>
2019-04-20 21:24:23 +0000

    Add basic intersection

--- src/render/sphere.rs
+++ src/render/sphere.rs
@@ -1,4 +1,4 @@
-use cgmath::Vector3;
+use cgmath::{Vector3, dot};
 use super::ray;
 use super::renderable::*;
 
@@ -9,6 +9,16 @@ pub struct Sphere<T> {
 
 impl Renderable for Sphere<f32> {
     fn intersection(self, ray: ray::Ray<f32>) -> bool {
-        false
+        let ray_offset = ray.origin - self.center;
+        let a: f32 = dot(ray.direction, ray.direction); // TODO test with magnitude2
+        let b = 2.0 * dot(ray_offset, ray.direction);
+        let c = dot(ray_offset, ray_offset) - self.radius.powi(2);
+        let discriminant = b * b - 4.0 * a * c;
+        if discriminant < 0.0 {
+            false
+        }
+        else {
+            true
+        }
     }
 }
