04b0b2795851d221a19477aa41fed0f01ac5e57d
blargg <tomjankauski@gmail.com>
2019-04-22 21:46:07 +0000

    image basic example

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 extern crate cgmath;
+extern crate image;
+
 mod render;
 
-use cgmath::Vector3;
-
+use image::{ImageBuffer, Rgb, Pixel};
 
 fn main() {
-    let v = Vector3::new(1.0, 2.0, 3.0);
-    println!("{}", v.x);
+    let mut img = ImageBuffer::from_fn(100, 100,
+                                       |_, _| Rgb::from_channels(255u8, 0, 0, 255));
+    img.put_pixel(10, 10, Rgb::from_channels(0, 255, 0, 255));
+    img.save("./test.png").expect("Failed to save to file.");
 }
