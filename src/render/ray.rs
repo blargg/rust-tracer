19da5067a603f0d117383d45b89095945d73de13
blargg <tomjankauski@gmail.com>
2019-04-22 17:02:55 +0000

    Basic sphere implementation and initial testing

--- src/render/ray.rs
+++ src/render/ray.rs
@@ -1,6 +1,6 @@
 extern crate cgmath;
 
-use cgmath::Vector3;
+use cgmath::{Vector3, BaseFloat, InnerSpace};
 
 pub struct Ray<T> {
     pub origin: Vector3<T>,
@@ -10,5 +10,48 @@ pub struct Ray<T> {
 impl<T> Ray<T> {
     pub const fn new(orig: Vector3<T>, dir: Vector3<T>) -> Ray<T> {
         Ray { origin: orig, direction: dir }
+    }
+}
+
+impl<T: BaseFloat> Ray<T> {
+    pub fn closestPoint(&self, p: Vector3<T>) -> Vector3<T> {
+        let p_trans = p - self.origin;
+        p_trans.project_on(self.direction) + self.origin
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use proptest::prelude::*;
+    use cgmath::vec3;
+    use super::*;
+
+    const delta: f64 = 0.00001;
+
+    fn st_vec3<T>(s: impl Strategy<Value=T> + Clone) -> impl Strategy<Value = Vector3<T>>
+        where T: Arbitrary
+    {
+        (s.clone(), s.clone(), s.clone()).prop_map(|(x, y, z)| vec3(x, y, z))
+    }
+
+    proptest! {
+        #[test]
+        fn closestOrig(point in st_vec3(-1000f64..1000f64)) {
+            let v0 = vec3(0.0, 0.0, 0.0);
+            let r: Ray<f64> = Ray::new(v0, point);
+            let closest = r.closestPoint(point);
+            let diff = (point-closest).magnitude();
+            prop_assert!(diff < delta, "diff = {}", diff);
+        }
+
+        #[test]
+        fn closestOnLine(orig in st_vec3(-1000f64..1000f64),
+                         point in st_vec3(-1000f64..1000f64),
+                         scale in -100f64..100f64) {
+            // point lies on the line that contains r: Ray
+            let r: Ray<f64> = Ray::new(orig, (point - orig) * scale);
+            let closest = r.closestPoint(point);
+            prop_assert!((point - closest).magnitude() < delta);
+        }
     }
 }
--- src/render/sphere.rs
+++ src/render/sphere.rs
@@ -1,4 +1,6 @@
-use cgmath::{Vector3, dot};
+
+use cgmath::{Vector3, vec3, dot};
+use cgmath::InnerSpace;
 use super::ray;
 use super::renderable::*;
 
@@ -8,7 +10,7 @@ pub struct Sphere<T> {
 }
 
 impl Renderable for Sphere<f32> {
-    fn intersection(self, ray: ray::Ray<f32>) -> bool {
+    fn intersection(&self, ray: ray::Ray<f32>) -> bool {
         let ray_offset = ray.origin - self.center;
         let a: f32 = dot(ray.direction, ray.direction); // TODO test with magnitude2
         let b = 2.0 * dot(ray_offset, ray.direction);
@@ -20,5 +22,25 @@ impl Renderable for Sphere<f32> {
         else {
             true
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use proptest::prelude::*;
+    use cgmath::abs_diff_eq;
+
+    use super::*;
+    use super::ray::Ray;
+
+    #[test]
+    fn intersection_test() {
+        let s: Sphere<f32> = Sphere{center: vec3(0.0, 0.0 ,0.0), radius: 5.0};;
+        let r: Ray<f32> = Ray{origin: vec3(0.0,0.0,0.0), direction: vec3(1.0,0.0,0.0)};
+
+        let r2: Ray<f32> = Ray::new(vec3(100.0, 0.0, 0.0), vec3(-1.0, 0.0, 0.0));
+
+        assert!(s.intersection(r));
+        //assert!(!(s.intersection(r2)));
     }
 }
